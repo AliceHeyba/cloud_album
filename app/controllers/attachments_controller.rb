b60ff941b5020ff0102a528f09fd75e9c66ce1ed
Philippe Paoli <philippe.paoli11@gmail.com>
2022-11-30 12:19:23 +0000

    pundit in place and individual view structure in plaCE

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,35 +1,62 @@
 class AttachmentsController < ApplicationController
-  before_action :set_event, only: %i[new create destroy]
+  before_action :set_event, only: %i[index new create show edit update destroy]
+  before_action :set_attachment, only: %i[show edit update destroy]
 
-
+  def index
+    @attachments = policy_scope(Attachment).where(event: @event)
+  end
 
   def new
     @attachment = Attachment.new
+    authorize @attachment
+  end
+
+  def show
+
   end
 
 
   def create
     @attachment = Attachment.new(attachment_params)
     @attachment.event = @event
-    if @attachment.save
-      redirect_to event_path(@event)
+    @attachment.user = current_user
+    authorize @attachment
+    if @attachment.save!
+      redirect_to event_attachment_path(@event, @attachment), notice: "Nice! Attachments uploaded succesfully."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @attachment.update(attachment_params)
+      redirect_to  redirect_to event_attachment_path(@event, @attachment), notice: "Attachment succesfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @attachment.destroy
-    redirect_to event_path(@event), status: :see_other
+    redirect_to event_attachments_path(@event), notice: "Attachment succesfully deleted"
   end
 
   private
 
   def set_event
-    @event = Event.find[params[:event_id]]
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_attachment
+    @attachment = Attachment.find(params[:id])
+    authorize @attachment
   end
 
   def attachment_params
-    params.require(:attachment).permit(:meta_picture, :caption, :posts)
+    params.require(:attachment).permit(:posts)
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,33 +1,53 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+
   def index
-    @events = Event.all
+    @events = policy_scope(Event).all
   end
 
   def new
     @event = Event.new
+    authorize @event
   end
 
   def show
-    @event = Event.find(params[:id])
+
   end
 
   def create
     @event = Event.new(event_params)
     @event.user = current_user
+    authorize @event
     if @event.save!
-      redirect_to event_path(@event)
+      redirect_to event_path(@event), notice: "Hurray! Event succesfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@event), notice: "Event succesfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
-    redirect_to events_path, status: :see_other
+    redirect_to events_path, notice: "Event succesfully deleted."
   end
 
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+    authorize @event
+  end
 
   def event_params
     params.require(:event).permit(:event_date, :event_name, :event_description, :attendee_email, :banner)
