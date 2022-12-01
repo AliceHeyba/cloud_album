80919d3f9b8c962d70733f356d213d4216a407ed
Alice Heber <alice.heber@gmail.com>
2022-12-01 09:28:55 +0000

    added cloudalbum name in config for cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,13 @@ User.destroy_all
 
 puts "creating event for Demo..."
 
-philippe = User.create!(first_name: "Philippe", last_name: "Paoli", email: "georges.john19890@gmail.com", password: "123456")
+alice = User.create!(first_name: "Alice", last_name: "Paoli", email: "alice19890@gmail.com", password: "123456")
 
-wagon_event = Event.create!(event_date: '2023-11-29', event_name: "Wedding Demo", event_description: "Demo Day Showcase", location: "Beirut", user: philippe)
-wagon_birthday = Event.create!(event_date: '2024-11-29', event_name: "Birthday Demo", event_description: "Demo Day Birthday", location: "Paris", user: philippe)
+wagon_event = Event.create!(event_date: '2023-11-29', event_name: "Wedding Demo", event_description: "Demo Day Showcase", location: "Beirut", user: alice)
+wagon_birthday = Event.create!(event_date: '2024-11-29', event_name: "Birthday Demo", event_description: "Demo Day Birthday", location: "Paris", user: alice)
 
 puts "event #{wagon_event.event_name} created!"
 
 puts "event #{wagon_birthday.event_name} created!"
 
-first_attachment = Attachment.create!(meta_picture: Date.today, caption: "beautiful moment", event_id: wagon_event.id, user: philippe)
+first_attachment = Attachment.create!(meta_picture: Date.today, caption: "beautiful moment", event_id: wagon_event.id, user: alice)
