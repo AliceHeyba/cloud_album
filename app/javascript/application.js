e0ddeac6b4a2f4e207407c4ccb1ac318e46761ac
Philippe Paoli <philippe.paoli11@gmail.com>
2022-12-01 13:58:03 +0000

    added like buttons on attachments show page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+// ./packs/application.js
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context));
