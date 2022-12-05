db13a6ee777357a424be8692637e03254ff088e3
Alice Heber <alice.heber@gmail.com>
2022-12-05 14:44:32 +0000

    validation first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, presence: true
   def fullname
     return "#{first_name} #{last_name}"
   end
