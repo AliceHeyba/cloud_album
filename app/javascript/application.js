dd58b2d6279fb182857144a4c640ed10e1ff6ffe
Jack Stephens <jack.stephens@me.com>
2022-12-05 17:52:33 +0000

    QR code toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,8 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 const application = Application.start()
 const context = require.context('../controllers', true, /\.js$/)
 application.load(definitionsFromContext(context));
+
+const button = document.querySelector(".qr-toggle")
+button.addEventListener("click", () => {
+  document.querySelector(".qr-code").classList.toggle("d-none")
+});
